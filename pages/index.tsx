205a2bad010a88b9fe6b9e7ea9cd3ac5eda5a7dd
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-18 19:42:32 -0500

    fetching data from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
 import type { NextPage } from 'next'
+import axios from 'axios';
+import { Video } from '../types'
 
+//*  Creating interface which will accept our data that we get sanity ( videos )
+interface IProps {
+  videos: Video[],
+}
 
+const Home = ({ videos } : IProps ) => {
 
-const Home: NextPage = () => {
+  console.log(videos);
   return (
     <h1 className='text-3xl font-bold underline'>
       Hello CanCan
     </h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    } 
+  }
 }
 
 export default Home
