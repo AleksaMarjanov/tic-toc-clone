e3cb85f5313bfd80dd3d969bfd94d6b76eca49dd
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-18 19:59:17 -0500

    implementing videoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import axios from 'axios';
 import { Video } from '../types'
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
 //*  Creating interface which will accept our data that we get sanity ( videos )
 interface IProps {
@@ -11,9 +13,15 @@ const Home = ({ videos } : IProps ) => {
 
   console.log(videos);
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello CanCan
-    </h1>
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+        <NoResults text={`No Videos`} />
+      )}
+    </div>
   )
 }
 
