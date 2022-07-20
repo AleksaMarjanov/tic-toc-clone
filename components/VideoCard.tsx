83d2c30a3492bcf3eec08de12b1ab0eff25d3897
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-20 17:58:57 -0500

    add play/mute buttons

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Video } from "./../types.d";
 import { NextPage } from "next";
-import  Image  from "next/image";
-import  Link  from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
 import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
@@ -12,6 +12,10 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-65">
       <div>
@@ -30,6 +34,58 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               </>
             </Link>
           </div>
+          <div>
+            <Link href="/">
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName}{" "}
+                  {`
+                `}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="lg:ml-2 flex gap-4 relative ">
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className="rounded-3xl "
+        >
+          <Link href="/">
+            <video
+              loop
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              src={post.video.asset.url}
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div className="flex gap-2 ">
+              {playing ? (
+                <button>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+              )}
+              {isVideoMuted ? (
+                <button>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
