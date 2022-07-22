20267b3c9154ef5ae7092a7f35a3fe8a57c26157
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-21 19:18:57 -0500

    implement google oauth functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
+import useAuthStore from '../store/authStore';
+
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from './../utils/index';
 
 const Navbar = () => {
+  const { userProfile, addUser } = useAuthStore();
+
+
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -22,6 +29,20 @@ const Navbar = () => {
         />
       </div>
       </Link>
+      
+      <div>
+        Search...
+      </div>
+
+      <div>
+      {userProfile ? (
+              <div>{userProfile.userName} </div>
+           
+        ): <GoogleLogin 
+        onSuccess={(response) => createOrGetUser(response, addUser)}
+        onError = {() => console.log('Error')}
+        />}
+      </div>
     </div>
   )
 }
