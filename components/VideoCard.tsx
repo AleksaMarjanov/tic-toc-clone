7b519779c5a8fbbda4ba5fc8f52b5826cfcb6646
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-22 20:54:48 -0500

    small changes to the videocard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -63,7 +63,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
         </div>
       </div>
 
-      <div className="lg:ml-2 flex gap-4 relative ">
+      <div className="lg:ml-20 flex gap-4 relative ">
         <div
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
@@ -73,7 +73,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             <video
               ref={videoRef}
               loop
-              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100 mb-3"
               src={post.video.asset.url}
             ></video>
           </Link>
