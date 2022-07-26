78d50b815004d020d626903c27ed602a86e456b1
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-25 21:18:55 -0500

    commenting functionality done

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -4,6 +4,7 @@ import React, { useEffect} from 'react'
 import { GoVerified } from 'react-icons/go'
 
 import useAuthStore from '../store/authStore'
+import { IUser } from '../types'
 
 const SuggestedAccounts = () => {
     const { fetchAllUsers, allUsers } = useAuthStore();
@@ -15,7 +16,33 @@ const SuggestedAccounts = () => {
 
     return (
     <div className='xl:border-b-2 border-gray-200 pb-4'>
-      <p>Suggested Accounts</p>
+      <p className='text-gray-500 text-semibold m-3 mt-4 hidden xl:block'>Suggested Accounts</p>
+
+      <div>
+      {allUsers.slice(0,6).map((user: IUser) => (
+        <Link href={`/profile/${user._id}`} key={user._id}>
+          <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
+              <div className='w-8 h-8 '>
+                <Image 
+                src={user.image}
+                width={34}
+                height={34}
+                className="rounded-full"
+                alt="user profile"
+                layout='responsive'
+                />
+              </div>
+              <div className='hidden xl:block'>
+              <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+                {user.userName.replaceAll(' ', '')}
+                <GoVerified className='text-blue-400'/>
+              </p>
+              <p className='capitalize text-gray-400 text-'>{user.userName}</p>
+              </div>
+          </div>
+        </Link>
+      ))}
+      </div>
     </div>
   )
 }
