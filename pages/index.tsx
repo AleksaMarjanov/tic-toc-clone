8922b7a01d11f728c82db5a5b8f6b6aa0886f329
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-07-27 18:33:21 -0500

    getting specific topic from discover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,46 @@
-import type { NextPage } from 'next'
-import axios from 'axios';
-import { Video } from '../types'
-import VideoCard from '../components/VideoCard';
-import NoResults from '../components/NoResults';
-import { BASE_URL } from '../utils';
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
+import { BASE_URL } from "../utils";
 
 //*  Creating interface which will accept our data that we get sanity ( videos )
 interface IProps {
-  videos: Video[],
+  videos: Video[];
 }
 
-const Home = ({ videos } : IProps ) => {
-
+const Home = ({ videos }: IProps) => {
   console.log(videos);
   return (
-    <div className='flex flex-col gap-10 videos h-full'>
+    <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id}/>
-        ))
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
       ) : (
         <NoResults text={`No Videos`} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = null;
+  
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data
-    } 
-  }
-}
+      videos: response.data,
+    },
+  };
+};
 
-export default Home
+export default Home;
